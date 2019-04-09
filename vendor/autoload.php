b5fca32ccc3aa82ba9b48fca0d7970c6807ef497
AliasWare <jacques.cofis@gmail.com>
2019-04-09 21:45:25 +0200

    init commit

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit26042ce6ee9f4d1638c2568d4f522cf5::getLoader();
+return ComposerAutoloaderInit8444617ef34920da642ea6dbc4ded97c::getLoader();
